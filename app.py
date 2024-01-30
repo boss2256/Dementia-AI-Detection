dd3c04aa69bb4aca7119f606ac2d0746e49f1f80
nesruler <72273305+boss2256@users.noreply.github.com>
2024-01-31 01:19:30 +0800

    changed app.run

--- app.py
+++ app.py
@@ -275,4 +275,6 @@ with app.app_context():
         print("Error creating database tables:", e)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
